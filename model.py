ab9f1562b99b42f68fdf0fe8fe54772c4dca261b
amrutha953 <amruthaayadhavponnu@gmail.com>
2026-05-25 17:26:32 +0530

    Updated CNN model with improvements

--- model.py
+++ model.py
@@ -1,20 +1,22 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
-# CNN Model for Potato Leaf Disease Detection
 def create_model():
     model = models.Sequential()
 
-    # Input + First Conv Layer
+    # Input + Conv Layer 1
     model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)))
+    model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling2D(2,2))
 
-    # Second Conv Layer
+    # Conv Layer 2
     model.add(layers.Conv2D(64, (3,3), activation='relu'))
+    model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling2D(2,2))
 
-    # Third Conv Layer
+    # Conv Layer 3
     model.add(layers.Conv2D(128, (3,3), activation='relu'))
+    model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling2D(2,2))
 
     # Flatten
@@ -22,15 +24,13 @@ def create_model():
 
     # Fully Connected Layer
     model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.5))   # NEW ADDITION
 
-    # Output Layer (3 classes)
+    # Output Layer
     model.add(layers.Dense(3, activation='softmax'))
 
     return model
 
 
-# Create model
 model = create_model()
-
-# Show model summary
 model.summary()
